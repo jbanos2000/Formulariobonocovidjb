047feebbef3626199aba327366ae26b6ad386bb5
Jesus <jesusbacha2000@gmail.com>
2020-10-19 02:49:03 -0500

    Startup data

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+using Formulariobonocovidjb.Data;
 namespace Formulariobonocovidjb
 {
     public class Startup
